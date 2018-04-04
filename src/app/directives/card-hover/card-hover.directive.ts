95063237d9600e6431cf8eb060efba6d03b831f3
= <nncl@live.com>
2018-04-03 22:56:32 -0300

    Adding custom configurations to ccCardHover directive

--- src/app/directives/card-hover/card-hover.directive.ts
+++ src/app/directives/card-hover/card-hover.directive.ts
@@ -1,10 +1,13 @@
-import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: '[appCardHover]'
 })
 export class CardHoverDirective {
     @HostBinding('class.card-outline-primary') private isHovering: boolean = false;
+    @Input('appCardHover') config: Object = {
+        querySelector: '.card-text'
+    };
 
     constructor(private el: ElementRef,
                 private renderer: Renderer2) {
@@ -14,13 +17,13 @@ export class CardHoverDirective {
     }
 
     @HostListener('mouseover') onMouseOver() {
-        const punchlineEl = this.el.nativeElement.querySelector('.card-text');
+        const punchlineEl = this.el.nativeElement.querySelector(this.config.querySelector);
         this.renderer.setStyle(punchlineEl, 'display', 'block');
         this.isHovering = true;
     }
 
     @HostListener('mouseout') onMouseOut() {
-        const punchlineEl = this.el.nativeElement.querySelector('.card-text');
+        const punchlineEl = this.el.nativeElement.querySelector(this.config.querySelector);
         this.renderer.setStyle(punchlineEl, 'display', 'none');
         this.isHovering = false;
     }
