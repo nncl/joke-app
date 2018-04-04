64f07d0aa960213cd358459e990a9fe547a21f2c
= <nncl@live.com>
2018-04-03 22:30:03 -0300

    Adding events and binding with HostBinding and HostListener decorators

--- src/app/directives/card-hover/card-hover.directive.ts
+++ src/app/directives/card-hover/card-hover.directive.ts
@@ -1,15 +1,28 @@
-import {Directive, ElementRef, Renderer} from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: '[appCardHover]'
 })
 export class CardHoverDirective {
+    @HostBinding('class.card-outline-primary') private isHovering: boolean = false;
 
     constructor(private el: ElementRef,
-                private renderer: Renderer) {
+                private renderer: Renderer2) {
 
         // el.nativeElement.style.backgroundColor = 'gray';
-        renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'gray'); // Better for other mobile devices
+        // renderer.setStyle(el.nativeElement, 'backgroundColor', 'gray'); // Better for other mobile devices
+    }
+
+    @HostListener('mouseover') onMouseOver() {
+        const punchlineEl = this.el.nativeElement.querySelector('.card-text');
+        this.renderer.setStyle(punchlineEl, 'display', 'block');
+        this.isHovering = true;
+    }
+
+    @HostListener('mouseout') onMouseOut() {
+        const punchlineEl = this.el.nativeElement.querySelector('.card-text');
+        this.renderer.setStyle(punchlineEl, 'display', 'none');
+        this.isHovering = false;
     }
 
 }
