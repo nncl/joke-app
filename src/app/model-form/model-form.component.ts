2b47eb092cdf21a0928a13bea0dddc439f898231
= <nncl@live.com>
2018-04-06 01:10:33 -0300

    Adding model driven forms and validation

--- src/app/model-form/model-form.component.ts
+++ src/app/model-form/model-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-model-form',
@@ -7,27 +7,48 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
     styleUrls: ['./model-form.component.scss']
 })
 export class ModelFormComponent implements OnInit {
+
     myForm: FormGroup;
+    firstName: FormControl;
+    lastName: FormControl;
+    email: FormControl;
+    password: FormControl;
+    language: FormControl;
 
     // langs: Array<string> = [];
     langs: string[] = [
         'English',
-        'Portuguese',
-        'French'
+        'French',
+        'Portuguese'
     ];
 
-    constructor() {
+    ngOnInit() {
+        this.createFormControls();
+        this.createForm();
     }
 
-    ngOnInit() {
+    createFormControls() {
+        this.firstName = new FormControl('', Validators.required);
+        this.lastName = new FormControl('', Validators.required)
+        this.email = new FormControl('', [
+            Validators.required, Validators.email
+        ]);
+        this.password = new FormControl('', [
+            Validators.required,
+            Validators.minLength(8)
+        ]);
+        this.language = new FormControl('');
+    }
+
+    createForm() {
         this.myForm = new FormGroup({
             name: new FormGroup({
-                firstName: new FormControl(),
-                lastName: new FormControl()
+                firstName: this.firstName,
+                lastName: this.lastName
             }),
-            email: new FormControl(),
-            password: new FormControl(),
-            language: new FormControl()
+            email: this.email,
+            password: this.password,
+            language: this.language
         });
     }
 
