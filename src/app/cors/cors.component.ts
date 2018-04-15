31a3204493209a36ea1cfbd621cb57b2e672d9cd
= <nncl@live.com>
2018-04-15 17:41:58 -0300

    More observables logic

--- src/app/cors/cors.component.ts
+++ src/app/cors/cors.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../search.service';
-import { Form } from '@angular/forms';
+import { Form, FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { SearchItem } from '../models/search-item';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
 
 @Component({
     selector: 'app-cors',
@@ -15,11 +21,23 @@ export class CorsComponent implements OnInit {
     loading: boolean = false;
     // results: SearchItem[];
     results: Observable<SearchItem[]>;
+    searchField: FormControl;
 
     constructor(private itunes: SearchService) {
     }
 
     ngOnInit() {
+        this.searchField = new FormControl();
+        this.results = this.searchField
+            .valueChanges
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .do(() => this.loading = true)
+            .switchMap(term => this.itunes.search(term))
+            .do(() => this.loading = false);
+
+        // .subscribe(item => console.log(item)); because of async pipe we
+        // don't have to sub/unsubscribe anymore
     }
 
     doSearch(term: string) {
